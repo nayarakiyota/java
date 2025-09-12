0342e46ffccffb85937bb49d0b8ff9d4d56746e6
Nayara Kiyota <nayprado23@gmail.com>
2025-09-12 11:21:11 -0300

    Projeto Operadores

---
+++ operadores/src/operadores/Calculadora.java
@@ -0,0 +1,38 @@
+package operadores;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		// Instanciar um objeto da classe scanner (entrada de dados)
+		
+		Scanner leia = new Scanner (System.in);
+		
+		// Definir as variáveis 
+		
+		double numero1, numero2;
+		
+		//Entrada de dados
+		
+		System.out.println("Digite o primeiro número: ");
+		numero1 = leia.nextDouble();
+				
+		System.out.println("Digite o segundo número: ");
+		numero2 = leia.nextDouble();
+		
+		// Efetuar os cálculos
+		
+		System.out.printf("%.2f + %.2f = %.2f%n", numero1, numero2, numero1 + numero2);
+		System.out.printf("%.2f - %.2f = %.2f%n", numero1, numero2, numero1 - numero2);
+		System.out.printf("%.2f * %.2f = %.2f%n", numero1, numero2, numero1 * numero2);
+		System.out.printf("%.2f / %.2f = %.2f%n", numero1, numero2, numero1 / numero2);
+		
+		// Operações Matemáticas com a Biblioteca Math
+		System.out.printf("%.2f ^ %.2f = %.2f%n", numero1, numero2, Math.pow(numero1, numero2));
+		System.out.printf("Raiz Quadrada de %.2f = %.2f%n", numero1, Math.sqrt(numero1));
+		
+	}
+
+}
